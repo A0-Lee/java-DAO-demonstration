b10f4de503a64cf421e4a40e046a1932d13c5b3b
unknown <PDS3D@THT_N03.tht.gmbh>
2020-03-27 21:35:35 +0000

    First commit v1.0

---
+++ src/main/java/al00952/com1028/assignment/App.java
@@ -0,0 +1,13 @@
+package al00952.com1028.assignment;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
